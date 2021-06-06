71c251c0507240cfb8d91b141b7432cfb6168637
Binh <thanhbinh710@gmail.com>
2021-06-07 01:06:43 +0800

    add login logout

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
         <p>This child application aims to show you how user authentication can be done</p>
         <br/>
         <br/>
+        <h2>
+          Currently user is <span class="pink-text" v-if="!isLoggedIn">not</span> logged in
+        </h2>
       </div>
     </div>
   </div>
@@ -15,6 +18,40 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      sticherAppDomain: process.env.VUE_APP_STITCHER_DOMAIN,
+      isLoggedIn: localStorage.getItem('jwt')
+    }
+  },
+  methods: {
+    listenForStitcherMessage(event) {
+      if (this.sticherAppDomain && this.sticherAppDomain === event.origin) {
+        // check if the message format is valid
+        if ( event.data && event.data.action) {
+          if (event.data.action === 'logout') {
+            this.logout()
+          } else if (event.data.action === 'login' && event.data.token) {
+            this.login(event.data.token)
+          }
+        }
+      }
+    },
+    login(token) {
+      this.isLoggedIn = true
+      localStorage.setItem('jwt', token)
+    },
+    logout() {
+      this.isLoggedIn = false
+      localStorage.removeItem('jwt')
+    },
+  },
+  mounted() {
+    window.addEventListener('message', this.listenForStitcherMessage)
+  },
+  beforeUnmount () {
+    window.removeEventListener('message', this.listenForStitcherMessage)
+  }
 }
 </script>
 
@@ -25,6 +62,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 60px;
+  background-color: azure;
+  height: 100vh;
+}
+.pink-text {
+  color: #FF4083;
 }
 </style>
